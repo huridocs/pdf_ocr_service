f339779a30f79a17bba59cbdf362c87508dceed4
Sergio Revilla <sergio.revilla@hey.com>
2022-07-08 12:04:27 +0200

    Add Sentry integration

--- src/app.py
+++ src/app.py
@@ -38,6 +38,7 @@ try:
     sentry_sdk.init(
         os.environ.get('SENTRY_OCR_DSN'),
         traces_sample_rate=0.1,
+        environment=os.environ.get('ENVIRONMENT', 'development')
 )
     app.add_middleware(SentryAsgiMiddleware)
 except Exception:
